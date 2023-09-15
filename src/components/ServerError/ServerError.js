e7c23ead2d02c1ceecc8f6fc8334ff35fe6f7a48
David Timofeev <dava.tim@yandex.ru>
2023-09-16 00:55:06 +0300

    b r2

--- src/components/ServerError/ServerError.js
+++ src/components/ServerError/ServerError.js
@@ -1,29 +1,4 @@
-// import { useNavigate } from "react-router-dom";
 import "./ServerError.css";
-// import { Link } from "react-router-dom";
-
-
-// function ServerError() {
-//   const NotFound = () => {
-//     const navigate = useNavigate();
-//     const goBack = () => {
-//       navigate(-1)
-//     }
-//   // }
-//     return (
-//       <div className="server-error">
-//         <h2 className="server-error__title">404</h2>
-//         <p className="server-error__message">Страница не найдена</p>
-//         <button //type={button} 
-//         onClick={goBack} text={'назад'} className="server-error__link" />
-//         {/* <Link to="/" className="server-error__link">
-//           Назад
-//         </Link> */}
-//       </div>
-//       // ........................
-//     );
-//   }
-// }
 
 import React from "react";
 import { useNavigate } from "react-router-dom";
